17f7949c4c1a0281db517ba7d8b28ed1dfce1205
zhangzhuoran997997 <63936820+zhangzhuoran997997@users.noreply.github.com>
2021-07-20 11:34:53 +0800

    this is to see how to use the pycharm to another branch commit

--- data.py
+++ data.py
@@ -1,2 +1,89 @@
 # Define the dataset
-# let me to see if there is something different
+import os
+import numpy as np
+import torch
+from PIL import Image
+
+class PennFudanDataset(torch.utils.data.Dataset):
+    def __inf__(self, root, transforms):
+        # set the root path of the dataset
+        self.root = root
+        # transfer the Data to tensor
+        self.transforms = transforms
+        # functions
+        # os.path.join(path1,path2..) -> put the paths together
+        # os.listdir(path) -> Return a list containing the names of the files in the directory.
+        # sorted(list) -> sort the things in the list
+        # load all image files and sort them
+        self.imgs = list(sorted(os.listdir(os.path.join(root,"PNGImages"))))
+        self.masks = list(sorted(os.listdir(os.path.join(root, "PedMasks"))))
+
+    def __getitem__(self, idx):
+        # load images and masks
+        img_path = os.path.join(self.root, "PNGImages", self.imgs[idx])
+        mask_path = os.path.join(self.root, "PedMasks", self.masks[idx])
+        # function
+        # open(path) -> load a file
+        # convert() -> change the format of the image
+        img = Image.open(img_path).convert("RGB")
+        # note that we haven't converted the mask to RGB,
+        # because each color corresponds to a different instance
+        # with 0 being background
+        mask = Image.open(mask_path)
+        # convert the PIL Image into a numpy array
+        mask = np.array(mask)
+        # instances are encoded as different colors
+        # get the instances in the mask: 0->background 1->instance1 2->instance2
+        # np.unique() -> 去除掉相同元素然后排序输出
+        obj_ids = np.unique(mask)
+        # first id is the background, so remove it
+        obj_ids = obj_ids[1:]
+
+        # split the color-encoded mask into a set of binary masks
+        # mask 与 每一个物体编号进行比较，eg1，2...，得到一个01的矩阵，代表着某个点是否是物体k
+        masks = mask == obj_ids[:, None, None]
+
+        # get bounding box coordinates for each mask
+        num_objs = len(obj_ids)
+        boxes = []
+        for i in range(num_objs):
+            # function
+            # np.where(array) -> output the coordinates where the elements
+            # in the array is not 0
+            pos = np.where(masks[i])
+            xmin = np.min(pos[1])
+            xmax = np.max(pos[1])
+            ymin = np.min(pos[0])
+            ymax = np.max(pos[0])
+            # function
+            # list.append(obj) -> add the obj to the list tail
+            # eg. list = [123,'zzr'] list.append('wlq') -> list = [123,'zzr','wlq']
+            boxes.append([xmin, ymin, xmax, ymax])
+
+        # convert everything into a torch.Tensor
+        boxes = torch.as_tensor(boxes, dtype=torch.float32)
+        # there is only one class
+        # function torch.ones(size,dtype) -> a tensor shaped size with all one
+        # size can be (2,3,4)
+        labels = torch.ones((num_objs,), dtype=torch.int64)
+        masks = torch.as_tensor(masks, dtype=torch.uint8)
+        image_id = torch.tensor([idx])
+        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+        # suppose all instances are not crowd
+        iscrowd = torch.zeros((num_objs,), dtype=torch.int64)
+
+        target = {}
+        target["boxes"] = boxes
+        target["labels"] = labels
+        target["masks"] = masks
+        target["image_id"] = image_id
+        target["area"] = area
+        target["iscrowd"] = iscrowd
+
+        if self.transforms is not None:
+            img, target = self.transforms(img, target)
+
+        return img, target
+
+    def __len__(self):
+        return len(self.imgs)
--- main.py
+++ main.py
@@ -2,15 +2,28 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
+import os
+import torch
+import numpy as np
+from PIL import Image
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    path = "D:/biancheng/python/python project/PennFudanPed/PennFudanPed/PedMasks/FudanPed00009_mask.png"
+    mask = Image.open(path)
+    mask = np.array(mask)
+    print(mask[137,381])
+    obj_ids = np.unique(mask)
+    print(obj_ids)
+    obj_ids = obj_ids[1:]
+    masks = mask == obj_ids[:, None, None]
+    print(masks.shape)
+    num_objs = len(obj_ids)
+    print(num_objs)
+    labels = torch.ones((2), dtype=torch.int64)
+    masks = torch.as_tensor(masks, dtype=torch.uint8)
+    print(masks,masks.shape)
+    tensor1 = torch.tensor(8)
+    tensor2 = torch.tensor([1,2])
+    print(tensor1 , tensor2)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
